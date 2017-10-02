ebe0e4df07303350184b95273f7c9290ad8b5283
柏大衛 <biede0@gmail.com>
2017-10-01 22:12:40 -0700

    feat(js-ts) Better JavaScript and TypeScript working together
    This includes a better example of working with JavaScript and TypeScript in the same project, with Karma and code coverage.

--- js-typescript2-karma/README.md
+++ js-typescript2-karma/README.md
@@ -1,21 +1,28 @@
 # JavaScript, TypeScript and Karma
 
-**NOTE**  This does not yet work fully.  In particular, this can generate code coverage for the TypeScript code, but not for the JavaScript code.
-
 ## About
-This is intended as a demonstration of working with JavaScript and TypeScript in the same codebase, and running tests with Karma.
+This is intended as a demonstration of working with JavaScript and TypeScript in the same codebase, running tests with Karma, and getting coverage reports out of it.
 
 ## Pieces
 * Typescript 2.5.2
 * npm 5
 * node 8.1.0
 * Karma 1.7.1
+* karma-typescript: 3.0.5
 
 ## To Do
 * From your terminal, make sure you are in the directory where this readme is found.
 * run `npm install`
-* run `npm run karma start`
+* run `npm run karma`
 * Look at the output in the `./temp` directory
+
+## Notes
+The keys to making this work is the 'karma-typescript' package, which provides the functionality needed to process TypeScript and deliver it to both Karma and coverage.
+
+One must specify the `allowJs` flag in the `.tsconfig.json` file, or this will not work.
+
+Note also that for reasons I do not yet understand, JavaScript test files show up in the coverage output, but TypeScript test files do not.
 
 ## Resources
 * https://www.npmjs.com/package/karma-typescript
+* https://github.com/adrianmarinica/karma-typescript-angularjs-sample This project helped a lot. Note, though, that it suffers the same limitation about test files (it just happens to only be using TypeScript for test code)
--- js-typescript2-karma/karma.conf.js
+++ js-typescript2-karma/karma.conf.js
@@ -2,97 +2,75 @@
 // Generated on Mon Sep 04 2017 15:56:38 GMT-0700 (PDT)
 
 module.exports = function(config) {
-  config.set({
+   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+      // base path that will be used to resolve all patterns (eg. files, exclude)
+      basePath: "",
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [
-       'jasmine',
-       'karma-typescript'
-    ],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      'source/**/*.js',
-      'source/**/*.ts'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'source/**/*.ts': ['karma-typescript'],
-      'source/**/*.js': ['coverage']
-   },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [
-       'progress',
-       'coverage',
-       'karma-typescript'
-    ],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-
-    // Not so obvious: https://www.npmjs.com/package/karma-typescript
-    karmaTypescriptConfig: {
-       reports: {
-         'html': './temp/coverage/html-report',
-         'json': './temp/coverage/json-report'
-      }
-    },
-
-    coverageReporter: {
-      dir : './temp/testCoverage/',
-      reporters: [
-         { type: 'json', file: 'coverage.json' },
-         { type: 'json-summary', file: 'coverage-summary.json' },
-         { type: 'html', subdir: 'report-html' },
-         { type: 'text', subdir: '.', file: 'text.txt' },
+      // frameworks to use
+      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+      frameworks: [
+         "jasmine",
+         "karma-typescript"  // while this is loaded either way, it needs to be listed here for some reason
       ],
-   },
 
-  })
+      // list of files / patterns to load
+      files: [
+         "source/**/*.js",
+         "source/**/*.ts"
+      ],
+
+      // list of files to exclude
+      exclude: [
+      ],
+
+      // preprocess matching files before serving them to the browser
+      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: {
+         "source/**/*.ts": ["karma-typescript"],
+         "source/**/*.js": ["karma-typescript"]
+      },
+
+      // test results reporter to use
+      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+      reporters: [
+         "progress",
+         "karma-typescript"
+      ],
+
+      // web server port
+      port: 9876,
+
+      // enable / disable colors in the output (reporters and logs)
+      colors: true,
+
+      // level of logging
+      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+      logLevel: config.LOG_INFO,
+
+      // enable / disable watching file and executing tests whenever any file changes
+      autoWatch: true,
+
+      // start these browsers
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+      browsers: ["Chrome"],
+
+      // Continuous Integration mode
+      // if true, Karma captures browsers, runs the tests and exits
+      singleRun: false,
+
+      // Concurrency level
+      // how many browser should be started simultaneous
+      concurrency: Infinity,
+
+      // https://www.npmjs.com/package/karma-typescript
+      karmaTypescriptConfig: {
+         tsconfig: "./tsconfig.json",
+         reports: {
+            "html": "./temp/coverage/html-report",
+            "json": "./temp/coverage/json-report"
+         }
+      },
+
+   })
 }
